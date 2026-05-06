08f36ccd20b27ffca7aaf249893a7467d3755b16
MAJIIDMN <muhammadnurmajiid22@gmail.com>
2026-05-06 23:42:33 +0700

    fix :double class destroyer (free{})

--- src/main.cpp
+++ src/main.cpp
@@ -19,7 +19,6 @@ int main(int argc, char* argv[]) {
         GuiApp app;
         if (app.init()) {
             app.run();
-            app.shutdown();
             return 0;
         }
         std::cerr << "GUI init failed, falling back to CLI.\n";
